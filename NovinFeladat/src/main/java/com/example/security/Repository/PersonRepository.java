a0c6228e4226865f29c188019c74fce0d09bba7f
KarakasDavid <112944704+KarakasDavid@users.noreply.github.com>
2023-08-21 20:16:52 +0200

    Delete NovinFeladat directory

--- NovinFeladat/src/main/java/com/example/security/Repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.security.Repository;
-
-import com.example.security.Model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-    Person findByUsername(String username);
-}
